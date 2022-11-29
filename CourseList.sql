f3bb7b1af7b82fa7873969aaf567104fbb936343
Aoife O'Kane Hackett <57546385+aoifeokh@users.noreply.github.com>
2022-11-29 12:05:56 +0000

    Delete CourseList.sql

--- CourseList.sql
+++
@@ -1,9 +0,0 @@
-CREATE VIEW dbo.CourseList
-AS
-SELECT  c.CourseName, c.CourseID, v.VendorName
-FROM Course as c
-INNER JOIN Vendor as v
-ON 
-c.VendorID = v.VendorID
-
-SELECT * FROM dbo.CourseList
